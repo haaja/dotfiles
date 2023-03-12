29dd701093e3a73f3d747e6649c9670f09b8314a
Janne Haapsaari <janne@joh.fi>
2023-03-12 19:49:05 +0200

    feat: add plugin lualine

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -53,6 +53,11 @@ return require('packer').startup(function(use)
   --use({ 'rose-pine/neovim', as = 'rose-pine' })
   use('bluz71/vim-nightfly-colors')
 
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
