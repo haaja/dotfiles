c57b1e2f75f7ddabec06c96efca2a12b33dd512c
Janne Haapsaari <janne@joh.fi>
2023-03-20 22:54:29 +0200

    fix: keep cursor in the middle when half page jumping

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -5,3 +5,6 @@ vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true });
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
