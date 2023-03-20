1446d440cf848632857685f4bfe1bb610558ecbc
Janne Haapsaari <janne@joh.fi>
2023-03-20 22:52:06 +0200

    fix: allow moving lines in visual mode

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true });
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
