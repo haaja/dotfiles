cf230e49ba6c77a12c4519c3fb1dba3749625171
Janne Haapsaari <haaja@iki.fi>
2023-08-05 22:18:28 +0300

    chore: drop parser install for help

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "go", "lua", "json", "yaml", "html", "css" },
+  ensure_installed = { "javascript", "typescript", "go", "lua", "json", "yaml", "html", "css" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
