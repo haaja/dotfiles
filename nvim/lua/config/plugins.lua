108736b7cfcc323f85a4c0995292c4a691c563b5
Janne Haapsaari <janne@joh.fi>
2023-09-23 20:17:34 +0300

    chore: bump telescope version

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -5,7 +5,7 @@ return require('packer').startup(function(use)
   -- My plugins here
   --
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
