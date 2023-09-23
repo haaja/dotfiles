195538809e84b42cab67e67ba959579615d8bdd0
Janne Haapsaari <janne@joh.fi>
2023-09-23 21:07:08 +0300

    fix: typo

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true });
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", "om '>+1<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
