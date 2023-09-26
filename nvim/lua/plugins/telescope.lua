60237015e7dd2315994029d3b4de7b6d300a6f8f
Janne Haapsaari <janne@joh.fi>
2023-09-26 21:15:57 +0300

    chore: adjust telescope pickers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,28 @@
-  return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.3',
-    dependencies = {
-      {'nvim-lua/plenary.nvim'},
-    },
-    config = function()
-      require('telescope').setup({
-        pickers = {
-          live_grep = {
-            hidden = true
-          },
-          find_files = {
-            hidden = true
-          }
-        }
-      })
-    end
-  }
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.3',
+  dependencies = {
+    {'nvim-lua/plenary.nvim'},
+  },
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules", "build", "dist", "yarn.lock"
+       },
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--smart-case", "--glob", "!**/.git/*" },
+          hidden = true
+        },
+        grep_string = {
+          hidden = true
+        },
+        live_grep = {
+          hidden = true
+        },
+      }
+    })
+  end
+}
