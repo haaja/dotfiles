9b72f7e2128e8748453d7c3bdba0798e0b7f5228
Janne Haapsaari <janne@joh.fi>
2024-05-20 13:05:10 +0300

    chore(nvim): dap: add keymap for run to cursor

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -94,6 +94,7 @@ return {
     vim.keymap.set('n', '<F2>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F3>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<F5>', dap.run_to_cursor, { desc = 'Debug: Run To Cursor' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
