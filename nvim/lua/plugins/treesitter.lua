9b3e020dd0343c4b88141fd5ff5e09487a168330
Janne Haapsaari <janne@joh.fi>
2024-05-21 09:42:30 +0300

    feat(nvim): keybindings for managing loops and conditionals

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -39,6 +39,10 @@ return {
               ['if'] = '@function.inner',
               ['ac'] = '@class.outer',
               ['ic'] = '@class.inner',
+              ['ai'] = '@conditional.outer',
+              ['ii'] = '@conditional.inner',
+              ['al'] = '@loop.outer',
+              ['il'] = '@loop.inner',
             },
           },
           move = {
