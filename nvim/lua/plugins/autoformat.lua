7711440c86974510cb656d07fcbbf9ee97e4aeb1
Janne Haapsaari <janne@joh.fi>
2024-05-24 08:38:06 +0300

    chore(nvim): add format typescript automatically

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -36,6 +36,7 @@ return {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
       },
     },
   },
